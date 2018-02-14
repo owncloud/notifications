d272b894fe3e00300565e05c0b168d63783c2e52
Phil Davis <phil@jankaritech.com>
2018-02-14 13:09:47 +0545

    Use common core integration test methods

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,29 +1,60 @@
 #!/usr/bin/env bash
 
-APP_NAME=notifications
+mkdir -p output
 
-APP_INTEGRATION_DIR=$PWD
-ROOT_DIR=${APP_INTEGRATION_DIR}/../../../..
-
-#cd ${ROOT_DIR}/build/integration
 composer install
-#cd ${APP_INTEGRATION_DIR}
 
-php -S localhost:8080 -t ${ROOT_DIR} &
+OC_PATH=../../../../
+CORE_INT_TESTS_PATH=tests/integration/
+OCC=${OC_PATH}occ
+
+SCENARIO_TO_RUN=$1
+HIDE_OC_LOGS=$2
+
+# avoid port collision on jenkins - use $EXECUTOR_NUMBER
+if [ -z "$EXECUTOR_NUMBER" ]; then
+    EXECUTOR_NUMBER=0
+fi
+PORT=$((8080 + $EXECUTOR_NUMBER))
+echo $PORT
+php -S localhost:$PORT -t ../../../../ &
 PHPPID=$!
 echo $PHPPID
 
-cp -R ./app ../../../notificationsintegrationtesting
-${ROOT_DIR}/occ app:enable notifications
-${ROOT_DIR}/occ app:enable notificationsintegrationtesting
+export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
 
-export TEST_SERVER_URL="http://localhost:8080/"
-${APP_INTEGRATION_DIR}/vendor/bin/behat -f junit -f pretty
+#Set up personalized skeleton
+$OCC config:system:set skeletondirectory --value="$(pwd)/$OC_PATH""$CORE_INT_TESTS_PATH""skeleton"
+
+#Set up mailhog to send emails
+$OCC config:system:set mail_domain --value="foobar.com"
+$OCC config:system:set mail_from_address --value="owncloud"
+$OCC config:system:set mail_smtpmode --value="smtp"
+$OCC config:system:set mail_smtphost --value="127.0.0.1"
+$OCC config:system:set mail_smtpport --value="1025"
+#We cannot set password with csrf enabled
+$OCC config:system:set csrf.disabled --value="true"
+
+#Enable needed app
+cp -R ./app ../../../notificationsintegrationtesting
+$OCC app:enable notifications
+$OCC app:enable notificationsintegrationtesting
+$OCC app:enable testing
+
+vendor/bin/behat --strict -f junit -f pretty $SCENARIO_TO_RUN
 RESULT=$?
 
 kill $PHPPID
 
-${ROOT_DIR}/occ app:disable notificationsintegrationtesting
+#Disable apps
+$OCC app:disable notifications
+$OCC app:disable notificationsintegrationtesting
+$OCC app:disable testing
 rm -rf ../../../notificationsintegrationtesting
 
+if [ -z $HIDE_OC_LOGS ]; then
+	tail "${OC_PATH}/data/owncloud.log"
+fi
+
+echo "runsh: Exit code: $RESULT"
 exit $RESULT
