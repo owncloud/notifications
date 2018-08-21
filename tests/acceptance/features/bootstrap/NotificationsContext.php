633f656f3dbe5935d3e9b6983d096c1da053c2a2
Phil Davis <phil@jankaritech.com>
2018-08-21 10:47:23 +0545

    PHP code style fixer changes

--- tests/acceptance/features/bootstrap/NotificationsContext.php
+++ tests/acceptance/features/bootstrap/NotificationsContext.php
@@ -77,7 +77,7 @@ class NotificationsContext implements Context {
 		//so it does not need to be mentioned in the table
 		$rows = $formData->getRows();
 		$rows[] = ["user", $user];
-		for ($rowCount = 0; $rowCount < count($rows); $rowCount ++) {
+		for ($rowCount = 0; $rowCount < \count($rows); $rowCount ++) {
 			$rows[$rowCount] = $this->featureContext->substituteInLineCodes($rows[$rowCount]);
 		}
 		$formData = new TableNode($rows);
@@ -164,7 +164,7 @@ class NotificationsContext implements Context {
 			200, $response->getStatusCode(),
 			"could not set notification option " . $response->getReasonPhrase()
 		);
-		$responseDecoded = json_decode($response->getBody());
+		$responseDecoded = \json_decode($response->getBody());
 		PHPUnit_Framework_Assert::assertEquals(
 			$responseDecoded->data->options->id, $user,
 			"Could not set notification option! " .
@@ -196,11 +196,11 @@ class NotificationsContext implements Context {
 		$lastNotificationIds = $this->notificationsCoreContext->getLastNotificationIds();
 		if ($firstOrLast === 'first') {
 			$this->notificationsCoreContext->setDeletedNotification(
-				end($lastNotificationIds)
+				\end($lastNotificationIds)
 			);
 		} else {
 			$this->notificationsCoreContext->setDeletedNotification(
-				reset($lastNotificationIds)
+				\reset($lastNotificationIds)
 			);
 		}
 		$this->featureContext->userSendsToOcsApiEndpoint(
