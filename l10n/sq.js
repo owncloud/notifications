b688dc20bcbc1c9fca24f8e7082dd1d19852fa51
Vincent Petry <pvince81@owncloud.com>
2018-09-04 07:54:55 +0200

    Backport translations from master, initial version

---
+++ l10n/sq.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "Njoftime",
+    "No notifications" : "Pa njoftime",
+    "Dismiss" : "Mos e merr parasysh",
+    "Admin notifications" : "Njoftime përgjegjësi",
+    "Unknown user session. It is not possible to set the option" : "Sesion përdoruesi i panjohur. S’është e mundur të caktohet mundësia",
+    "Option not supported" : "Mundësi që nuk mbulohet",
+    "Saved" : "U ruajt",
+    "Incomplete data" : "Të dhëna të paplota",
+    "Do not notify via mail" : "Mos njofto me email",
+    "Notify only when an action is required" : "Njofto vetëm kur lypset një veprim",
+    "Notify about all events" : "Njoftohuni mbi krejt veprimtaritë",
+    "Choose an option" : "Zgjidhni një mundësi",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "Për më tepër të dhëna, shihni <a href=\"%s\">%s</a> te %s",
+    "See %s on %s for more information" : "Për më tepër të dhëna, shihni %s te %s",
+    "Mail Notifications" : "Njoftime Me Email",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Mund të zgjidhni të njoftoheni me email për veprimtaritë. Disa veprimtari janë informative, të tjera lypin një veprim (b.f. pranoje/hidhe poshtë). Përzgjidhni më poshtë parapëlqimin tuaj:",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "S’qe e mundur të merrej sesioni juaj. Ju lutemi, provoni ringarkin e faqes ose bëni daljen dhe hyni sërish",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "Që të jeni në gjendje të merrni njoftime me email, lypset të përcaktoni një adresë email për llogarinë tuaj."
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/th_TH.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "การแจ้งเตือน",
+    "No notifications" : "ไม่มีการแจ้งเตือน",
+    "Dismiss" : "ยกเลิก",
+    "Admin notifications" : "การแจ้งเตือนสำหรับผู้ดูแลระบบ",
+    "Unknown user session. It is not possible to set the option" : "ไม่รู้จักเซสชันผู้ใช้ ไม่สามารถตั้งค่าตัวเลือกได้",
+    "Option not supported" : "ไม่รองรับตัวเลือก",
+    "Saved" : "บันทึกแล้ว",
+    "Incomplete data" : "ข้อมูลไม่สมบูรณ์",
+    "Do not notify via mail" : "ไม่ต้องแจ้งเตือนผ่านทางเมล",
+    "Notify only when an action is required" : "แจ้งเตือนเมื่อจำเป็นต้องมีการดำเนินการเท่านั้น",
+    "Notify about all events" : "แจ้งเตือนเกี่ยวกับเหตุการณ์ทั้งหมด",
+    "Choose an option" : "เลือกตัวเลือก",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "ดูข้อมูลเพิ่มเติมเกี่ยวกับ <a href=\"%s\">%s</a> ได้ที่ %s ",
+    "See %s on %s for more information" : "ดูข้อมูลเพิ่มเติมเกี่ยวกับ %s ได้ที่ %s",
+    "Mail Notifications" : "การแจ้งเตือนทางเมล",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "คุณสามารถเลือกรับการแจ้งเตือนเกี่ยวกับเหตุการณ์ต่างๆ ผ่านทางอีเมลได้ เหตุการณ์บางเหตุการณ์อาจมีข้อมูล เหตุการณ์อื่นๆ อาจจำเป็นต้องมีการดำเนินการ (เช่น ยอมรับ/ปฏิเสธ) เลือกการตั้งค่าของคุณด้านล่างนี้:",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "ไม่สามารถรับเซสชันของคุณได้ กรุณาลองโหลดเพจนี้ใหม่ หรือออกจากระบบและเข้าสู่ระบบใหม่อีกครั้ง",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "เพื่อให้สามารถรับการแจ้งเตือนทางเมลได้ คุณจำเป็นต้องระบุที่อยู่อีเมลสำหรับบัญชีของคุณ"
+},
+"nplurals=1; plural=0;");
