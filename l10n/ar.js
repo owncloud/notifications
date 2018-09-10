427590c463e2773ff83bc2592b94bbde05190edf
ownCloud Jenkins <jenkins@owncloud.com>
2018-09-10 02:11:17 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ar.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "الإشعارات",
+    "No notifications" : "لا يوجد اشعارات",
+    "Dismiss" : "تراجع",
+    "Admin notifications" : "اشعارات المدير",
+    "Unknown user session. It is not possible to set the option" : "جلسة اتصال غير معروف. من غير الممكن تحديد الخيار",
+    "Option not supported" : "الخيار غير مدعوم",
+    "Saved" : "تم الحفظ",
+    "Incomplete data" : "البيانات غير مكتملة",
+    "Do not notify via mail" : "لا ترسل اشعارات عن طريق الايميل ",
+    "Notify only when an action is required" : "نبهني فقط عندما تحتاج الي اجراء",
+    "Notify about all events" : " اشعار حول / عن كافة الاحداث",
+    "Choose an option" : "اختر احد الخيارات",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "اطلع علي <a href=\"%s\">%s</a> في %s للمزيد من المعلومات ",
+    "See %s on %s for more information" : "اطلع علي %s في %s للمزيد من المعلومات",
+    "Mail Notifications" : "اشعارات البريد",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "تستطيع ان تختار الاشعار حول / عن كافة الاحداث عن طريق الايميل . بعض الاحداث تكون اخبارية و توعوية . واحداث اخرى تحتاج الي اتخاذ اجراء ( علي سبيل المثال   القبول / الرفض ). حدد اختيارك من التالي :",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "لم يكن من الممكن اسعادة جلسة الاتصال الخاصة بك. فضلا, حاول تحديث الصفحة او قم بتسجيل الخروج ثم تسجيل الدخول مره اخرى",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "حتي تتمكن من استلام الاشعارات علي البريد الالكتروني يجب عليك تحديد بريد الالتكروني لحسابك "
+},
+"nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
