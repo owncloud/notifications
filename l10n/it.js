41b95defa214c4d0d534f3c1527f44d27290dd33
ownClouders <devops@owncloud.com>
2018-10-13 05:03:27 +0000

    [tx] updated from transifex

---
+++ l10n/it.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "Notifiche",
+    "No notifications" : "Nessuna notifica",
+    "Dismiss" : "Annulla",
+    "Admin notifications" : "Notifiche amministrative",
+    "Unknown user session. It is not possible to set the option" : "Sessione utente sconosciuta. Non è possibile impostare l'opzione",
+    "Option not supported" : "Opzione non supportata",
+    "Saved" : "Salvato",
+    "Incomplete data" : "Dati incompleti",
+    "Do not notify via mail" : "Non notificare tramite posta elettronica",
+    "Notify only when an action is required" : "Notifica solo quando un'azione è richiesta",
+    "Notify about all events" : "Notifiche per tutti gli eventi",
+    "Choose an option" : "Scegli un'opzione",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "Vedi <a href=\"%s\">%s</a> su %s per ulteriori informazioni",
+    "See %s on %s for more information" : "Vedi %s su %s per ulteriori informazioni",
+    "Mail Notifications" : "Notifiche di posta elettronica",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Puoi scegliere, se ricevere le notifiche via mail, per gli eventi.Alcuni eventi sono informativi, altri richiedono un azione (per esempio accetta/rifiuta). Seleziona la tua preferenza di seguito: ",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Non è stato possibile prendere la tua sessione. Per cortesia, prova a fare un refresh della pagina oppure esci e rientra.",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "Per poter ricevere le notifiche di posta è necessario specificare un indirizzo email per il tuo account."
+},
+"nplurals=2; plural=(n != 1);");
