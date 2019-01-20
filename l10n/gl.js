89eb3c3bb37ec3421091acdd78b6da0bd2e81ebd
ownClouders <devops@owncloud.com>
2019-01-20 05:05:07 +0000

    [tx] updated from transifex

---
+++ l10n/gl.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "Notificacións",
+    "No notifications" : "Non hai notificacións",
+    "Dismiss" : "Desbotar",
+    "Admin notifications" : "Notificacións de administración",
+    "Unknown user session. It is not possible to set the option" : "Sesión de usuario descoñecida. Non é posíbel estabelecer a opción.",
+    "Option not supported" : "Opción non admitida",
+    "Saved" : "Gardado",
+    "Incomplete data" : "Datos incompletos",
+    "Do not notify via mail" : "Non notificar por correo",
+    "Notify only when an action is required" : "Notificar só cando se require unha acción",
+    "Notify about all events" : "Notificar sobre todos os eventos",
+    "Choose an option" : "Escolla unha opción",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "Vexa <a href=\"%s\">%s</a> en %s para obter máis información",
+    "See %s on %s for more information" : "Vexa %s en %s para obter máis información ",
+    "Mail Notifications" : "Notificacións por correo",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Pode escoller ser notificado sobre os eventos mediante o correo. Algúns eventos son informativos, outros requiren unha acción (como aceptar/declinar). Seleccione embaixo a súa preferencia:",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Non foi posíbel obter a súa sesión. Tente cargar de novo a páxina ou saia e volva acceder.",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "Para poder recibir notificacións por correo debe especificar un enderezo de correo para a súa conta."
+},
+"nplurals=2; plural=(n != 1);");
