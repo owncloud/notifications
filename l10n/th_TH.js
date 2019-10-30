ef4a607a58e8d3422795d0b4bdca68fdfbbcbb20
ownClouders <devops@owncloud.com>
2019-10-30 12:50:55 +0000

    [tx] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -9,15 +9,15 @@ OC.L10N.register(
     "Option not supported" : "ไม่รองรับตัวเลือก",
     "Saved" : "บันทึกแล้ว",
     "Incomplete data" : "ข้อมูลไม่สมบูรณ์",
-    "Do not notify via mail" : "ไม่ต้องแจ้งเตือนผ่านทางเมล",
+    "Do not notify via mail" : "ไม่ต้องแจ้งเตือนผ่านทางอีเมล",
     "Notify only when an action is required" : "แจ้งเตือนเมื่อจำเป็นต้องมีการดำเนินการเท่านั้น",
     "Notify about all events" : "แจ้งเตือนเกี่ยวกับเหตุการณ์ทั้งหมด",
     "Choose an option" : "เลือกตัวเลือก",
     "See <a href=\"%s\">%s</a> on %s for more information" : "ดูข้อมูลเพิ่มเติมเกี่ยวกับ <a href=\"%s\">%s</a> ได้ที่ %s ",
     "See %s on %s for more information" : "ดูข้อมูลเพิ่มเติมเกี่ยวกับ %s ได้ที่ %s",
-    "Mail Notifications" : "การแจ้งเตือนทางเมล",
-    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "คุณสามารถเลือกรับการแจ้งเตือนเกี่ยวกับเหตุการณ์ต่างๆ ผ่านทางอีเมลได้ เหตุการณ์บางเหตุการณ์อาจมีข้อมูล เหตุการณ์อื่นๆ อาจจำเป็นต้องมีการดำเนินการ (เช่น ยอมรับ/ปฏิเสธ) เลือกการตั้งค่าของคุณด้านล่างนี้:",
+    "Mail Notifications" : "การแจ้งเตือนทางอีเมล",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "คุณสามารถเลือกรับการแจ้งเตือนเกี่ยวกับกิจกรรมต่างๆผ่านทางอีเมลได้ กิจกรรมบางเหตุการณ์อาจจำเป็นต้องมีการดำเนินการ (เช่น ยอมรับ/ปฏิเสธ) ซึ่งสามารถตั้งค่าได้จากด้านล่างนี้:",
     "It was not possible to get your session. Please, try reloading the page or logout and login again" : "ไม่สามารถรับเซสชันของคุณได้ กรุณาลองโหลดเพจนี้ใหม่ หรือออกจากระบบและเข้าสู่ระบบใหม่อีกครั้ง",
-    "To be able to receive mail notifications it is required to specify an email address for your account." : "เพื่อให้สามารถรับการแจ้งเตือนทางเมลได้ คุณจำเป็นต้องระบุที่อยู่อีเมลสำหรับบัญชีของคุณ"
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "เพื่อให้สามารถรับการแจ้งเตือนทางอีเมลได้ คุณจำเป็นต้องระบุที่อยู่อีเมลสำหรับบัญชีของคุณ"
 },
 "nplurals=1; plural=0;");
