d84d9dbb1253f1b22ef62321faeab1f65ab89eda
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-23 02:27:43 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -13,6 +13,8 @@ OC.L10N.register(
     "Notify only when an action is required" : "Sadece bir işlem gerektiren durumlarda bildirim gönder",
     "Notify about all events" : "Tüm etkinlikler hakkında bildirim gönder",
     "Choose an option" : "Bir seçenek seçin",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "Daha fazla bilgi için %s üzerinde <a href=\"%s\">%s</a> bakın",
+    "See %s on %s for more information" : "Daha fazla bilgi için %s üzerinde %s bakın",
     "Mail Notifications" : "E-posta Bildirimleri",
     "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Etkinlikler hakkında e-posta bildirimleri almayı seçebilirsiniz. Bazı bildirimler sadece bilgilendirme amaçlı, bazıları ise bir işlem gerektirebilir (onaylama, redetme vb.) Tercihlerinizi aşağıdan belirleyin:",
     "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Oturumunuza ulaşılamadı. Lütfen sayfayı yeniden yüklemeyi deneyin ya da çıkış yapıp tekrar giriş yapın.",
