ff52c9b60dc4c58f7e66300b9a0ad47050e4f553
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-01 00:05:49 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "Benachrichtigungen",
+    "No notifications" : "Keine Benachrichtigungen",
+    "Dismiss" : "Ausblenden",
+    "Admin notifications" : "Admin-Benachrichtigungen",
+    "Unknown user session. It is not possible to set the option" : "Unbekannte Benutzer-Sitzung. Die Einstellung konnte nicht gesetzt werden. ",
+    "Option not supported" : "Einstellung wird nicht unterstützt. ",
+    "Saved" : "Gespeichert",
+    "Incomplete data" : "Unvollständige Daten",
+    "Do not notify via mail" : "Nicht per E-Mail benachrichtigen",
+    "Notify only when an action is required" : "Nur benachrichtigen wenn Aktion notwendig ist",
+    "Notify about all events" : "Über alle Ereignisse benachrichtigen",
+    "Choose an option" : "Wähle eine Option",
+    "Hello," : "Hallo,",
+    "See <a href=\"%s\">%s</a> on %s for more information" : "Siehe <a href=\"%s\">%s</a> in %s für weitere Informationen",
+    "See %s on %s for more information" : "Bitte %s auf %s ansehen, für weitere Informationen",
+    "Mail Notifications" : "E-Mail-Benachrichtigungen",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Du kannst dich über Ereignisse per E-Mail benachrichtigen lassen. Einige davon sind informativ, andere erfordern eine Aktion (wie Annehmen oder Zurückweisen). Wähle deine Einstellungen hier:",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Es war nicht möglich deine Sitzung zu identifizieren. Bitte lade die Seite neu oder melde dich erneut an.",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "Um Benachrichtigungen zu erhalten ist es notwendig eine E-Mail-Adresse für deinen Benutzerkonto anzugeben. Gehe zu deinen Einstellungen (Allgemein) um die Adresse einzutragen."
+},
+"nplurals=2; plural=(n != 1);");
