bf7aa6ba5a491d251848e5a3d882d4c44848b56f
Thomas Müller <thomas.mueller@tmit.eu>
2020-12-10 00:48:02 +0000

    [tx] updated from transifex

---
+++ l10n/pt_PT.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "Notificações",
+    "No notifications" : "Sem notificações",
+    "Dismiss" : "Rejeitar",
+    "Unknown user session. It is not possible to set the option" : "Sessão de utilizador desconhecida. Não é possível definir a opção",
+    "Option not supported" : "Opção não suportada",
+    "Saved" : "Guardada",
+    "Incomplete data" : "Dados incompletos",
+    "Do not notify via mail" : "Não notificar por correio eletrónico",
+    "Notify only when an action is required" : "Notificar só quando uma ação for pedida",
+    "Notify about all events" : "Notificar sobre todos os eventos",
+    "Hello," : "Olá,",
+    "Mail Notifications" : "Notificações por Email",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Pode escolher ser notificado via email sobre eventos. Alguns eventos são informativos, outros requerem uma ação (como aceitar/declinar). Selecione abaixo a sua preferência: ",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Não foi possível obter a sua sessão. Por favor tente recarregar a página ou saia e entre novamente",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "Para pode receber notificações por email é requerido que especifique um endereço e email para a sua conta."
+},
+"nplurals=2; plural=(n != 1);");
