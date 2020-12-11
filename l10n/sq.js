4ac5ee0fd9ed1c1c13cac5260ffe2d68ca118283
Michael Barz <mbarz@owncloud.com>
2020-12-11 15:52:48 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -18,7 +18,7 @@ OC.L10N.register(
     "See %s on %s for more information" : "Për më tepër të dhëna, shihni %s te %s",
     "Mail Notifications" : "Njoftime Me Email",
     "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Mund të zgjidhni të njoftoheni me email për veprimtaritë. Disa veprimtari janë informative, të tjera lypin një veprim (b.f. pranoje/hidhe poshtë). Përzgjidhni më poshtë parapëlqimin tuaj:",
-    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "S’qe e mundur të merrej sesioni juaj. Ju lutemi, provoni ringarkin e faqes ose bëni daljen dhe hyni sërish",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "S’qe e mundur të merrej sesioni juaj. Ju lutemi, provoni ringarkimin e faqes ose bëni daljen dhe hyni sërish",
     "To be able to receive mail notifications it is required to specify an email address for your account." : "Që të jeni në gjendje të merrni njoftime me email, lypset të përcaktoni një adresë email për llogarinë tuaj."
 },
 "nplurals=2; plural=(n != 1);");
