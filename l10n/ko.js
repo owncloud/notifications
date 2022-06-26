8dc7127fc2ebb8dadf3b3d50ba71be0ea44ce1cf
ownClouders <devops@owncloud.com>
2022-06-26 00:10:46 +0000

    [tx] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "notifications",
+    {
+    "Notifications" : "알림",
+    "Dismiss" : "닫기",
+    "Admin notifications" : "관리자 알림",
+    "Unknown user session. It is not possible to set the option" : "알 수 없는 사용자 세션입니다. 설정을 변경할 수 없습니다.",
+    "Option not supported" : "옵션이 지원되지 않습니다.",
+    "Saved" : "저장됨",
+    "Incomplete data" : "손실 데이터",
+    "Do not notify via mail" : "메일을 통해 알리지 않습니다.",
+    "Notify only when an action is required" : "액션이 필요할 때에만 알립니다.",
+    "Notify about all events" : "모든 이벤트에 대해 알림",
+    "Hello," : "안녕하세요,",
+    "Mail Notifications" : "메일 알림",
+    "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "메일을 통해 알림받을 이벤트를 선택할 수 있습니다. 일부 이벤트는 유용하고, 다른 이벤트는 액션(예: 수락/거절)을 필요로 합니다. 아래에서 당신이 선호하는 것을 고르세요.",
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "세션을 불러올 수 없습니다. 페이지를 다시 로딩하거나 로그아웃 후 다시 로그인해주시기 바랍니다.",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "메일 알림을 받으려면 계정의 이메일 주소를 지정해야 합니다."
+},
+"nplurals=1; plural=0;");
