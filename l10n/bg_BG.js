f8b1ac5ccb3a089ef5289b95e3ed53110f4980ef
ownClouders <devops@owncloud.com>
2022-11-10 07:07:42 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -3,14 +3,14 @@ OC.L10N.register(
     {
     "Notifications" : "Известия",
     "No notifications" : "Няма известия",
-    "Dismiss" : "Затвори",
+    "Dismiss" : "Отхвърляне",
     "Admin notifications" : "Администраторски известия",
-    "Unknown user session. It is not possible to set the option" : "Неразпозната потребителска сесия. Не е възможно да зададете опцията.",
+    "Unknown user session. It is not possible to set the option" : "Неизвестна потребителска сесия. Не е възможно да се зададе опцията",
     "Option not supported" : "Неподдържана опция",
     "Saved" : "Запазено",
-    "Incomplete data" : "Незавършени данни",
-    "Do not notify via mail" : "Не известявай чрез имейл",
-    "Notify only when an action is required" : "Известявай само при нужда от извършване на действие",
+    "Incomplete data" : "Непълни данни",
+    "Do not notify via mail" : "Без уведомяване чрез имейл",
+    "Notify only when an action is required" : "Уведомяване, само когато е необходимо действие",
     "Notify about all events" : "Известяване за всички събития",
     "Choose an option" : "Изберете опция",
     "Hello," : "Здравейте,",
@@ -18,7 +18,7 @@ OC.L10N.register(
     "See %s on %s for more information" : "Вижте %s на %s за повече информация",
     "Mail Notifications" : "Известия по имейл",
     "You can choose to be notified about events via mail. Some events are informative, others require an action (like accept/decline). Select your preference below:" : "Може да изберете да бъдете известени за събития по имейл. Някои известия са информативни, други изискват някакво действие (като приемане/отказ). Изберете вашите предпочитания.",
-    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Невъзможно получаване на сесия. Моля, опитайте да презаредите страницата или се разлогнете и се логнете отново.",
-    "To be able to receive mail notifications it is required to specify an email address for your account." : "За да можете да получавате известия по имейл е необходимо да зададете имейл адреса си."
+    "It was not possible to get your session. Please, try reloading the page or logout and login again" : "Невъзможно е да се осъществи сесията ви. Моля, опитайте да презаредите страницата или да излезете и да влезете отново",
+    "To be able to receive mail notifications it is required to specify an email address for your account." : "За да можете да получавате известия за поща, е необходимо да посочите имейл адрес във вашия профил."
 },
 "nplurals=2; plural=(n != 1);");
